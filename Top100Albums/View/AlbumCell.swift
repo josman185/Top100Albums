abf7cb87244bc998ff418b6ba057ca41f2325278
Jose <josman185@gmail.com>
2020-03-14 22:43:30 -0500

    Refactoring AlbumCell class

---
+++ Top100Albums/View/AlbumCell.swift
@@ -0,0 +1,92 @@
+//
+//  AlbumCell.swift
+//  Top100Albums
+//
+//  Created by Jose Vargas on 13/03/20.
+//  Copyright © 2020 joscompany. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+    
+    var albumLabel = UILabel()
+    var artistLabel = UILabel()
+    var thumbImage = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(thumbImage)
+        addSubview(albumLabel)
+        addSubview(artistLabel)
+        
+        configureImageView()
+        configureAlbumLabel()
+        configureArtistLabel()
+        setThumbImageConstraints()
+        setAlbumLabelConstraits()
+        setArtisLabelConstarint()
+    }
+    
+    func set(album: Album) {
+        thumbImage.setCustomImage(album.artworkUrl100)
+        albumLabel.text = album.name
+        artistLabel.text = album.artistName
+    }
+    
+    func configureImageView() {
+        thumbImage.layer.cornerRadius = 10
+        thumbImage.clipsToBounds     = true
+    }
+    
+    func configureAlbumLabel() {
+        albumLabel.numberOfLines = 0
+        albumLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        albumLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func configureArtistLabel() {
+        artistLabel.numberOfLines = 0
+        artistLabel.font = UIFont.italicSystemFont(ofSize: 16.0)
+        artistLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func setThumbImageConstraints() {
+        thumbImage.translatesAutoresizingMaskIntoConstraints                                        = false
+        thumbImage.centerYAnchor.constraint(equalTo: centerYAnchor).isActive                        = true
+        thumbImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12).isActive           = true
+        thumbImage.heightAnchor.constraint(equalToConstant: 90).isActive                           = true
+        thumbImage.widthAnchor.constraint(equalTo: thumbImage.heightAnchor, multiplier: 1.0).isActive = true
+    }
+    
+    func setAlbumLabelConstraits() {
+        albumLabel.translatesAutoresizingMaskIntoConstraints                                         = false
+        albumLabel.topAnchor.constraint(equalTo: thumbImage.topAnchor).isActive = true
+        albumLabel.leadingAnchor.constraint(equalTo: thumbImage.trailingAnchor, constant: 20).isActive  = true
+        albumLabel.heightAnchor.constraint(equalToConstant: 40).isActive                            = true
+        albumLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive          = true
+    }
+    
+    func setArtisLabelConstarint() {
+        artistLabel.translatesAutoresizingMaskIntoConstraints                                          = false
+        artistLabel.leadingAnchor.constraint(equalTo: thumbImage.trailingAnchor, constant: 20).isActive  = true
+        artistLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12).isActive          = true
+        artistLabel.heightAnchor.constraint(equalToConstant: 20).isActive                            = true
+        artistLabel.topAnchor.constraint(equalTo: albumLabel.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
