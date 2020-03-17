8ec8006ad34ae0df1246b6561ce6f626a553532b
Jose <josman185@gmail.com>
2020-03-17 17:57:28 -0500

    Eliminating some whitespace

--- Top100Albums/Extensions/ViewExtension.swift
+++ Top100Albums/Extensions/ViewExtension.swift
@@ -10,10 +10,10 @@ import UIKit
 
 extension UIView {
     func pin(to superview: UIView) {
-        translatesAutoresizingMaskIntoConstraints                             = false
-        topAnchor.constraint(equalTo: superview.topAnchor).isActive          = true
-        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive   = true
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
         trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive     = true
+        bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
     }
 }
