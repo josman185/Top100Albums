69f02485e1e8cfb31a6f5bcb722e841cd1f949e0
Jose <josman185@gmail.com>
2020-03-17 17:54:29 -0500

    adding some marks :)

--- Top100Albums/Controller/AlbumDetailViewController.swift
+++ Top100Albums/Controller/AlbumDetailViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class AlbumDetailViewController: UIViewController {
 
+    // MARK: - Properties
     var detailView: AlbumDetail? { return view as? AlbumDetail }
     var album: Album?
     
+    // MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setAlbumDetail()
         iTunesButtonAction()
     }
     
+    // MARK: - SetDetailData
     func setAlbumDetail() {
         self.title = album?.artistName
         self.view = AlbumDetail(frame: UIScreen.main.bounds)
         detailView?.set(detail: album)
     }
     
+    // MARK: - Actions
     func iTunesButtonAction() {
         #if targetEnvironment(simulator)
             self.showAlert(titleStr: "We Apologize", messageStr: "Use a device to enjoy all features.", style: .cancel)
